f57c771a557dcd8d16149252e541ceb701c5af7c
bobc701 <bobc@4bcx.com>
2019-12-05 18:52:39 -0500

    Initial commit

---
+++ BcxbXf/BcxbXf/BcxbXf/Models/BoxScoreListViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BCX.BCXB;
+
+namespace BcxbXf.Models {
+
+   class BoxScoreListViewModel : INotifyPropertyChanged { 
+
+      public ObservableCollection<CBatBoxSet> BatterBoxVis { get; set; }
+      //public ObservableCollection<CBatBoxSet> BatterBoxHome { get; set; }
+      public ObservableCollection<CPitBoxSet> PitcherBoxVis { get; set; }
+      //public ObservableCollection<CPitBoxSet> PitcherBoxHome { get; set; }
+
+      public CBatBoxSet BatterBoxVis_tot { get; set; }
+      private CGame mGame;
+      public string VisName { get { return mGame.t[0].nick; } }
+      public string HomeName { get { return mGame.t[1].nick; } }
+
+
+      public BoxScoreListViewModel(CGame g, int side = 0) {
+      // ------------------------------------------------------
+         mGame = g;
+         CBatter bat;
+         CPitcher pit;
+         int bx, px;
+
+         BatterBoxVis = new ObservableCollection<CBatBoxSet>();
+         var bsTot = new CBatBoxSet() { boxName = "Total"};
+         for (int i = 1; i <= CGame.SZ_BAT; i++) {
+            bx = g.t[side].xbox[i];
+            if (bx == 0) break;
+            bat = g.t[side].bat[bx];
+            BatterBoxVis.Add(bat.bs);
+            bsTot += bat.bs; //Operator overload! 
+         }
+         BatterBoxVis.Add(bsTot); //This is the totals row.
+
+
+         //BatterBoxHome = new ObservableCollection<CBatBoxSet>();
+         //for (int i = 1; i <= CGame.SZ_BAT; i++) {
+         //   bx = g.t[1].xbox[i];
+         //   if (bx == 0) break;
+         //   bat = g.t[1].bat[bx];
+         //   BatterBoxHome.Add(bat.bs);
+         //}
+
+
+         PitcherBoxVis = new ObservableCollection<CPitBoxSet>();
+         for (int i = 1; i <= CGame.SZ_PIT; i++) {
+            px = g.t[side].ybox[i];
+            if (px == 0) break;
+            pit = g.t[side].pit[px];
+            pit.ps.boxName = pit.pname;
+            /*for (int j=1; j<=10; j++)*/ PitcherBoxVis.Add(pit.ps);
+         }
+
+
+         //PitcherBoxHome = new ObservableCollection<CPitBoxSet>();
+         //for (int i = 1; i <= CGame.SZ_PIT; i++) {
+         //   px = g.t[1].xbox[i];
+         //   if (px == 0) break;
+         //   pit = g.t[1].pit[px];
+         //   PitcherBoxHome.Add(pit.ps);
+         //}
+
+         //var bs1 = new CBatBoxSet();
+         foreach (var bs in BatterBoxVis) bsTot = bsTot + bs; //Operator overload!
+         BatterBoxVis_tot = bsTot;
+
+      }
+
+      public event PropertyChangedEventHandler PropertyChanged;
+      void OnPropertyChanged([CallerMemberName] string propertyName = "") {
+
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+   }
+}
---
+++ BcxbXf/BcxbXf/BcxbXf/Views/PickTeamsPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using BCX.BCXCommon;
+
+namespace BcxbXf {
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class PickTeamsPage : ContentPage {
+
+
+      public string[] SelectedTeams { get; set; } = new string[2]; //<-- This is how selections are passed back to MainPage.
+
+
+      public PickTeamsPage(List<string> teamList) {
+      // ---------------------------------------------------------------
+         InitializeComponent();
+
+         pickerVis.ItemsSource = teamList;
+         pickerHome.ItemsSource = teamList;
+         //pickerVis.ItemDisplayBinding = new Binding("TeamTag");
+         //pickerHome.ItemDisplayBinding = new Binding("TeamTag");
+      }
+
+
+      private void btnUse_Clicked(object sender, EventArgs e) {
+      // -------------------------------------------------------
+         //DisplayAlert("", "Use these teams", "OK");
+         SelectedTeams[0] = (string)pickerVis.SelectedItem;
+         SelectedTeams[1] = (string)pickerHome.SelectedItem;
+         Navigation.PopModalAsync();
+      }
+
+
+      private void btnCanc_Clicked(object sender, EventArgs e) {
+      // -------------------------------------------------------
+         //DisplayAlert("", "Cancel", "OK");
+         SelectedTeams[0] = "";
+         SelectedTeams[1] = "";
+         Navigation.PopModalAsync();
+      }
+
+      private void picker_IndexChanged(object sender, EventArgs e) {
+      // ------------------------------------------------------------------
+         btnUse.IsEnabled = (pickerVis.SelectedIndex >= 0 && pickerHome.SelectedIndex >= 0);
+
+      }
+   }
+}
