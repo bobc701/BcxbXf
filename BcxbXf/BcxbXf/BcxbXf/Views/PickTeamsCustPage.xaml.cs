a3ac283d5259d0a68c03b7033b75c33e6640e923
Bob C <bobc@4bccx.com>
2021-04-19 17:16:59 -0400

    W.I.P.

--- BcxbXf/BcxbXf/BcxbXf/Views/PickTeamsCustPage.xaml.cs
+++ BcxbXf/BcxbXf/BcxbXf/Views/PickTeamsCustPage.xaml.cs
@@ -37,14 +37,15 @@ namespace BcxbXf.Views {
 
          pickerVis.SelectedIndexChanged +=
             (object sender, EventArgs e) => {
-               btnUse.IsEnabled = true;
-               //pickerVis.NewPickedTeam.Year != 0 && pickerHome.NewPickedTeam.Year != 0;
+               btnUse.IsEnabled =
+               pickerVis.SelectedItem is not null && pickerHome.SelectedItem is not null;
             };
 
          pickerHome.SelectedIndexChanged +=
             (object sender, EventArgs e) => {
-               btnUse.IsEnabled = true;
-               //pickerVis.NewPickedTeam.Year != 0 && pickerHome.NewPickedTeam.Year != 0;
+               btnUse.IsEnabled =
+               pickerVis.SelectedItem is not null && pickerHome.SelectedItem is not null;
+
             };
 
          Debug.WriteLine($"--------- TeamCache.Count in PickTeamsPage constructor: {DataAccess.TeamCache.Count}");
@@ -57,11 +58,9 @@ namespace BcxbXf.Views {
 
       private async void btnUse_Clicked(object sender, EventArgs e) {
          // -------------------------------------------------------
-         //DisplayAlert("", "Use these teams", "OK");
-         //fPickPrep.SelectedTeams[0] = pickerVis.NewPickedTeam; //(CTeamRecord)pickerVis.SelectedItem; 
-         //fPickPrep.SelectedTeams[1] = pickerHome.NewPickedTeam;//(CTeamRecord)pickerHome.SelectedItem;
+         fPickPrep.SelectedTeams[0] = (CTeamRecord)pickerVis.SelectedItem;
+         fPickPrep.SelectedTeams[1] = (CTeamRecord)pickerHome.SelectedItem;
 
-         //Dismiss?.Invoke();
          await Navigation.PopToRootAsync();
 
       }
@@ -75,14 +74,14 @@ namespace BcxbXf.Views {
          Navigation.PopAsync();
       }
 
-      private void picker_IndexChanged(object sender, EventArgs e) {
-         // ------------------------------------------------------------------
-         //btnUse.IsEnabled =
-         //   pickerVis.NewPickedTeam.Year != 0 &&
-         //   pickerHome.NewPickedTeam.Year != 0;
-         //(pickerVis.SelectedItem != null) && ((CTeamRecord)(pickerVis.SelectedItem)).Year != 0 &&
-         //(pickerHome.SelectedItem != null) && ((CTeamRecord)(pickerHome.SelectedItem)).Year != 0;
-      }
+      //private void picker_IndexChanged(object sender, EventArgs e) {
+      //   // ------------------------------------------------------------------
+      //   //btnUse.IsEnabled =
+      //   //   pickerVis.NewPickedTeam.Year != 0 &&
+      //   //   pickerHome.NewPickedTeam.Year != 0;
+      //   //(pickerVis.SelectedItem != null) && ((CTeamRecord)(pickerVis.SelectedItem)).Year != 0 &&
+      //   //(pickerHome.SelectedItem != null) && ((CTeamRecord)(pickerHome.SelectedItem)).Year != 0;
+      //}
 
       public void StartActivity() { // #3000.01
                                     // --------------------------------
